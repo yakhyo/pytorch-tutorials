7c010a332f10748b88c981456359505d04fd7a1e
yakhyo <yakhyo9696@gmail.com>
2021-05-01 23:39:01 +0900

    Update 02-deep-residual-nn

---
+++ tutorials/02-intermediate/02-deep-residual-nn/main.py
@@ -0,0 +1,196 @@
+import torch
+import torch.nn as nn
+import torchvision
+import torchvision.transforms as transforms
+from tqdm import tqdm
+
+# ========================================= #
+#         Load and normalize the data       #
+# ========================================= #
+
+transform = transforms.Compose([
+    transforms.Pad(4),
+    transforms.ToTensor(),
+    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+])
+
+# ========================================= #
+#        Hyper Parameters and Device        #
+# ========================================= #
+
+batch_size = 32
+epochs = 5
+learning_rate = 0.001
+momentum = 0.9
+num_classes = 10
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+train_data = torchvision.datasets.CIFAR10(
+    root='data',
+    train=True,
+    download=True,
+    transform=transform
+)
+
+test_data = torchvision.datasets.CIFAR10(
+    root='data',
+    train=False,
+    download=True,
+    transform=transform
+)
+
+train_loader = torch.utils.data.DataLoader(
+    dataset=train_data,
+    batch_size=batch_size,
+    shuffle=True
+)
+
+test_loader = torch.utils.data.DataLoader(
+    dataset=test_data,
+    batch_size=batch_size,
+    shuffle=False
+)
+
+classes = ('plane', 'car', 'bird', 'cat',
+           'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
+
+def conv3x3(in_planes, out_planes, stride=1):
+    """3x3 convolution with padding"""
+    return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride, padding=1, bias=False)
+
+
+def conv1x1(in_planes, out_planes, stride=1):
+    """1x1 convolution"""
+    return nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=stride, bias=False)
+
+
+class BasicBlock(nn.Module):
+    def __init__(self, inplanes, planes, stride=1, downsample=None):
+        super(BasicBlock, self).__init__()
+        self.conv1 = conv3x3(inplanes, planes, stride)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv2 = conv3x3(planes, planes)
+        self.bn2 = nn.BatchNorm2d(planes)
+        self.downsample = downsample
+        self.stride = stride
+
+    def forward(self, x):
+        identity = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+
+        if self.downsample is not None:
+            identity = self.downsample(x)
+
+        out += identity
+        out = self.relu(out)
+
+        return out
+
+
+class ResNet(nn.Module):
+
+    def __init__(self, block, layers, num_classes=1000):
+        super(ResNet, self).__init__()
+
+        self.in_planes = 64
+
+        self.conv1 = conv3x3(3, self.in_planes)
+        self.bn = nn.BatchNorm2d(self.in_planes)
+        self.relu = nn.ReLU(inplace=True)
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+
+        self.layer1 = self.make_layer(block, 64, layers[0])
+        self.layer2 = self.make_layer(block, 128, layers[1], stride=2)
+        self.layer3 = self.make_layer(block, 256, layers[2], stride=2)
+        self.layer4 = self.make_layer(block, 512, layers[3], stride=2)
+
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        self.fc = nn.Linear(512, num_classes)
+
+    def make_layer(self, block, planes, blocks, stride=1):
+        downsample = None
+        if stride != 1 or self.in_planes != planes:
+            downsample = nn.Sequential(
+                conv1x1(self.in_planes, planes, stride),
+                nn.BatchNorm2d(planes)
+            )
+
+        layers = []
+        layers.append(block(self.in_planes, planes, stride, downsample))
+        self.in_planes = planes
+        for _ in range(1, blocks):
+            layers.append(block(self.in_planes, planes))
+
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+
+        x = self.avgpool(x)
+        x = torch.flatten(x, 1)
+        x = self.fc(x)
+
+        return x
+
+
+net = ResNet(BasicBlock, [2, 2, 2, 2]).to(device)
+
+# ========================================= #
+#         Optimizer and Loss function       #
+# ========================================= #
+
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.SGD(net.parameters(), lr=learning_rate, momentum=momentum)
+
+# ========================================= #
+#          Train and Test the Model         #
+# ========================================= #
+for epoch in range(epochs):
+    progress_bar = tqdm(train_loader, nrows=len(train_loader))
+    for i, (images, labels) in enumerate(progress_bar):
+        inputs, labels = images.to(device), labels.to(device)
+
+        # zero the parameter gradients
+        optimizer.zero_grad()
+
+        # forward + backward + optimize
+        outputs = net(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        # print statistics
+    print(f'Epochs: {epoch + 1}/{epochs} Loss: {loss:.4f}')
+
+print('Finished Training')
+
+print('Started Testing')
+correct = 0
+total = 0
+
+with torch.no_grad():
+    for images, labels in tqdm(test_loader, nrows=len(test_loader)):
+        images, labels = images.to(device), labels.to(device)
+        outputs = net(images)
+        _, predicted = torch.max(outputs.data, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+
+print(f'Accuracy of the network on test data: {100 * correct / total}')
