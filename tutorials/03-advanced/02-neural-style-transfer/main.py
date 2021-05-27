3adbf83fcbbbf0b67f3d76d80c9148f3f82b4a86
yakhyo <yakhyo9696@gmail.com>
2021-05-27 04:49:10 -0400

    update

---
+++ tutorials/03-advanced/02-neural-style-transfer/main.py
@@ -0,0 +1,7 @@
+"""
+Please refer to the link below:
+https://github.com/yakhyo/Fast-Neural-Style-Transfer
+
+Perceptual Losses for Real-Time Style Transfer and Super-Resolution
+https://arxiv.org/abs/1603.08155
+"""
