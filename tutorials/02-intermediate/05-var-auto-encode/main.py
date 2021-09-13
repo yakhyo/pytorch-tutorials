20d14ef4fe12b74f4c44b22331e9b7ade11a743e
Yakhyokhuja Valikhujaev <yakhyo9696@gmail.com>
2021-09-13 01:54:18 -0700

    Update main.py

--- tutorials/02-intermediate/05-var-auto-encode/main.py
+++ tutorials/02-intermediate/05-var-auto-encode/main.py
@@ -27,7 +27,7 @@ device = torch.device("cuda" if args.cuda else "cpu")
 kwargs = {'num_workers': 1, 'pin_memory': True} if args.cuda else {}
 
 # ================================================================ #
-#                      Prepare  and Dataloader                     #
+#                  Prepare Dataset and Dataloader                  #
 # ================================================================ #
 
 train_loader = torch.utils.data.DataLoader(
