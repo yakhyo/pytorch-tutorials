3360a63a8771cdbdccb8e392f2b31479df3c9f4d
yakhyo <yakhyo9696@gmail.com>
2023-10-30 20:21:52 +0900

    chore: Creating notebooks for tutorials

--- tutorials/01-basics/01-quickstart/main.py
+++ tutorials/01-basics/01-quickstart/main.py
@@ -60,8 +60,14 @@ for images, labels in test_dataloader:
 #                           Creating Models                        #
 # ================================================================ #
 
-# Get CPU or GPU device for training
-device = ('cuda' if torch.cuda.is_available() else 'cpu')
+# Get cpu, gpu or mps device for training.
+device = (
+    "cuda"
+    if torch.cuda.is_available()
+    else "mps"
+    if torch.backends.mps.is_available()
+    else "cpu"
+)
 print(f'Using {device} device')
 
 
