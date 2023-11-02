3859a6fc49e82ef4dd0536d250a46bf7e6736959
yakhyo <yakhyo9696@gmail.com>
2023-11-03 05:52:29 +0900

    chore: Some udpates

--- tutorials/01-basics/05-neural-network/main.py
+++ tutorials/01-basics/05-neural-network/main.py
@@ -8,8 +8,14 @@ from torchvision import datasets, transforms
 #                       Get Device for Training                    #
 # ================================================================ #
 
-device = ('cuda' if torch.cuda.is_available() else 'cpu')
-print(f'Using {device} device.')
+device = (
+    "cuda"
+    if torch.cuda.is_available()
+    else "mps"
+    if torch.backends.mps.is_available()
+    else "cpu"
+)
+print(f'Using {device} device')
 
 
 # ================================================================ #
